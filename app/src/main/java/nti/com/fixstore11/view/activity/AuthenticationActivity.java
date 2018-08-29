dfaebd92060eb64f725df88b5382499b2d66bf04
fred <farid123fathy@gmail.com>
2018-08-29 13:55:18 +0200

    first

--- app/src/main/java/nti/com/fixstore11/view/activity/AuthenticationActivity.java
+++ app/src/main/java/nti/com/fixstore11/view/activity/AuthenticationActivity.java
@@ -1,15 +1,80 @@
 package nti.com.fixstore11.view.activity;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.nio.file.Files;
 
 import nti.com.fixstore11.R;
 
 public class AuthenticationActivity extends AppCompatActivity {
+    ConstraintLayout selection, login;
+    EditText phoneT;
+    Button nextBTN;
+    CheckBox clientCheck, HandmanCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_authentication);
+
+        init();
+        actions();
     }
+
+    private void init() {
+        selection = findViewById(R.id.selectLayout);
+        login = findViewById(R.id.LoginLayout);
+        phoneT = findViewById(R.id.etPhone);
+        clientCheck = findViewById(R.id.ClientCheck);
+        HandmanCheck = findViewById(R.id.HandmanCheck);
+        nextBTN = findViewById(R.id.nextBTN);
+        selection.setVisibility(View.INVISIBLE);
+    }
+
+    private void actions() {
+        nextBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /// get phone to firebase
+                login.setVisibility(View.INVISIBLE);
+                selection.setVisibility(View.VISIBLE);
+            }
+        });
+        clientCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    startActivity(new Intent(AuthenticationActivity.this, MainActivity.class));
+                    HandmanCheck.setChecked(false);
+                }
+            }
+        });
+        HandmanCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    startActivity(new Intent(AuthenticationActivity.this, LoginActivity.class));
+                    clientCheck.setChecked(false);
+                }
+            }
+        });
+
+
+    }
+
+
 }
--- app/src/main/java/nti/com/fixstore11/view/activity/SplashActivity.java
+++ app/src/main/java/nti/com/fixstore11/view/activity/SplashActivity.java
@@ -5,28 +5,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import nti.com.fixstore11.R;
 
 public class SplashActivity extends AppCompatActivity {
-    ImageView splashImage;
-    private final int SPLASH_TIME = 3 * 1000;
 
 
+    private Handler handler =new Handler();
+    ProgressBar bar;
+    TextView counttv;
+
+    private int status=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        splashImage = (ImageView) findViewById(R.id.ev_splash_image);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
+        setContentView(R.layout.activity_splash);
+
+        bar=findViewById(R.id.pd);
+        counttv=findViewById(R.id.tvcount);
+
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (status<100)
+                {
+                    status+=1;
+                    try
+                    {
+                        Thread.sleep(60);
+                    } catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            bar.setProgress(status);
+                            counttv.setText(status+" ");
+                        }
+                    });
+                }
+                startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
                 finish();
+
             }
-        }, SPLASH_TIME);
+        }).start();
+
+//        splashImage = (ImageView) findViewById(R.id.ev_splash_image);
+//
+//        new Handler().postDelayed(new Runnable() {
+//            public void run() {
+//                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, SPLASH_TIME);
 
 
 
