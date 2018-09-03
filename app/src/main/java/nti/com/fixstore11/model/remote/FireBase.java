a538b6a0d1856b818b0c8b0cf5ad5fb0b6012190
mohamedtaha251 <m.ahmed.taha251@gmail.com>
2018-09-03 16:35:06 +0300

    fixstore15

--- app/src/main/java/nti/com/fixstore11/model/remote/FireBase.java
+++ app/src/main/java/nti/com/fixstore11/model/remote/FireBase.java
@@ -2,7 +2,7 @@ package nti.com.fixstore11.model.remote;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;       
 import android.util.Log;
 import android.widget.Toast;
 
