b675d1bf86b7d2f6c1e4083d57e136b14d33f263
Mohamed Taha <m.ahmed.taha251@gmail.com>
2018-09-09 23:40:46 +0200

    add missing file

---
+++ app/src/main/java/nti/com/fixstore11/view/Interfaces/OrderDetailActivityView.java
@@ -0,0 +1,12 @@
+package nti.com.fixstore11.view.Interfaces;
+
+
+import nti.com.fixstore11.model.entities.User;
+
+/**
+ * Created by qrdz4162 on 2/7/2018.
+ */
+
+public interface OrderDetailActivityView {
+
+}
